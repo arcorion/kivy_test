08e6f8c700517109397818f35ab102a64cdc5402
Christopher Lee Crader <67856679+arcorion@users.noreply.github.com>
2025-05-29 18:56:36 -0700

    Build outline of huskontroller touchpanel start

--- kivy_test.py
+++ kivy_test.py
@@ -1,18 +1,86 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.graphics import *
+from kivy.lang.builder import Builder
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.widget import Widget
+from pathlib import Path
+from random import choice
+import platform
 
 
-class Panel(BoxLayout):
-    button = Button(text="I am a button")
+operating_system = platform.system()
+match operating_system:
+    case 'Linux':
+        Config.set('graphic', 'fullscreen', 'auto')
+        Config.set('graphics', 'width', '800')
+        Config.set('graphics', 'height', '480')
+        Config.set('graphics', 'borderless', '1')
+        Window.show_cursor = False
+    case 'Windows':
+        Window.size = (800, 480)
+    case _:
+        Exception("Not a supported OS")
 
-class Test(App):
+Builder.load_file('kivy_test.kv')
+
+class TouchPanel(FloatLayout):
+    def __init__(self, **kwargs):
+        super(TouchPanel, self).__init__(**kwargs)
+
+    def get_background(self):
+        match operating_system:
+            case 'Linux':   
+                image_directory = Path.cwd() / 'images' / 'backgrounds'
+            case 'Windows':
+
+                image_directory = Path('C:/Users/Christopher/OneDrive - UW/Documents/Projects/kivy_test/images/backgrounds')
+            case _:
+                Exception("Not a supported OS")        
+        
+        background_list = [x for x in image_directory.iterdir()]
+        return str(choice(background_list))
+
+
+class TopBar(Widget):
+    def __init__(self, **kwargs):
+        super(TopBar, self).__init__(**kwargs)
+
+
+class PowerButton(ToggleButton):
+    pass
+
+class InputOption(ToggleButton):
+    pass
+
+class InputButton(InputOption):
+    pass
+
+class TouchButton(ToggleButton):
+    pass
+
+class MuteButton(TouchButton):
+    pass
+
+class OptionButton(TouchButton):
+    pass
+
+class FreezeButton(TouchButton):
+    pass
+
+class BlankButton(TouchButton):
+    pass
+
+class Huskontroller(App):
     def build(self):
-        return Panel()
-    
+        return TouchPanel()
+
 
 if __name__ == "__main__":
     """
     Run me, my dear friend!
     """
-    Test().run()
+    Huskontroller().run()
